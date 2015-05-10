b4c4aff03b9374cfca32ebb34b5eb8ff6907b18a
nerolaser <arial.morales@gmail.com>
2015-05-10 16:59:27 -0600

    Se arreglo los valores constantes para cada atributo. Las poblaciones iniciales, por el momento no se crean debido a que se necesita establecer los valores iniciales.

--- Villager.h
+++ Villager.h
@@ -7,6 +7,26 @@ class Villager {
 public:
     Villager();
     virtual ~Villager();
+    int getVida();
+    int getAtaque();
+    int getDefensa();
+    int getHpRegen();
+    int getMagia();
+    int getBlot();
+    int getSupersticion();
+    int getVelocidad();
+    int getInteligencia();
+private:
+    const int vida; //5k
+    const int ataque; //1k
+    const int defensa; //1k
+    const int hpRegen; //500
+    // las runas calcular con la paridad del numero de cromosomas en 1, si es par, es 0 y si es impar es 1;
+    const int magia; //1k
+    const int blot; //1k
+    const int supersticion; //3k; 1k la primera eda;
+    const int velocidad; //100
+    const int inteligencia; //5k ; 1.5k segunda eda, 3.5k la tercera eda;
 };
 
 } /* namespace std */
--- Vllager.cpp
+++ Vllager.cpp
@@ -2,13 +2,57 @@
 
 namespace std {
 
-Villager::Villager() {
+Villager::Villager(): vida(5000), ataque(1000), defensa(1000), hpRegen(500), magia(1000), blot(100), supersticion(3000),velocidad(100), inteligencia(5000) {
     // TODO Auto-generated constructor stub
-
+//    this->vida = 5000;
+//    this->ataque = 1000;
+//    this->defensa = 1000;
+//    this->hpRegen = 500;
+//    this ->magia = 1000;
+//    this->blot = 100;
+//    this->supersticion = 3000;
+//    this->velocidad = 100;
+//    this->inteligencia = 5000;
 }
 
 Villager::~Villager() {
     // TODO Auto-generated destructor stub
+}
+
+ int Villager::getVida(){
+    return this->vida;
+}
+
+int Villager::getAtaque(){
+    return this->ataque;
+}
+
+int Villager::getDefensa(){
+    return this->defensa;
+}
+
+int Villager::getHpRegen(){
+    return this->hpRegen;
+}
+
+int Villager::getMagia(){
+    return this->magia;
+}
+
+int Villager::getBlot(){
+    return this->blot;
+}
+
+int Villager::getSupersticion(){
+    return this->supersticion;
+}
+
+int Villager::getVelocidad(){
+    return this->velocidad;
+}
+
+int Villager::getInteligencia(){
+    return this->inteligencia;
 }
 
 } /* namespace std */
